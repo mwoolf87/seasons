a024a033a49e263a4e6481a4c185dfb624ede656
mwoolf87 <michael.j.woolf@gmail.com>
2020-05-12 16:58:46 -0400

    added conditional rendering

--- src/index.js
+++ src/index.js
@@ -6,17 +6,25 @@ class App extends React.Component {
     super(props);
     //THIS IS THE ONLY TIME we do direct assignment
     // to this.setState
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
   render() {
-    return <div>Latitude: {this.state.lat} </div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading...</div>;
   }
 }
 
