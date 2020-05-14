964ac7798adf4c35614d4dfb01b3017108c07756
mwoolf87 <michael.j.woolf@gmail.com>
2020-05-14 11:32:07 -0400

    styled loading screen prior to season display

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay.js";
+import Loader from "./loader.js";
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
 
@@ -17,7 +18,7 @@ class App extends React.Component {
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading...</div>;
+    return <Loader />;
   }
 }
 
